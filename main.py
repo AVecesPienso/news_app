6e81803f391ed0f54f31a1846c02b126d2c7720b
AVecesPienso <a.escarcega.r@outlook.com>
2026-03-18 23:17:49 -0600

    feat: implement game search with id, name and small summary

--- main.py
+++ main.py
@@ -18,3 +18,39 @@ def get_token(c_id, c_secret):
     response.raise_for_status()
     token = response.json().get("access_token")
     return token
+
+def get_game_list(game_name, token, client_id):
+    game_request_path = "https://api.igdb.com/v4/games"
+    headers = {
+        "Client-ID": client_id,
+        "Authorization": f"Bearer {token}"
+    }
+    body = f'search "{game_name}"; fields id, name, summary; limit 10;'
+    response = requests.post(game_request_path, headers=headers, data=body)
+
+    if response.status_code == 200:
+        response_data = response.json()
+        if response_data:
+            return response_data
+    else:
+        print(f"Search error: {response.status_code}")
+        return None
+    
+def main():
+   token = get_token(client_id, client_secret)
+   game = input("Enter game to search: ")
+   games_list = get_game_list(game, token, client_id)
+
+   if games_list:
+       print(f"\n{len(games_list)} games found:")
+
+       for game in games_list:
+        name = game.get('name', 'N/A')
+        summary = game.get('summary', 'N/A')
+        game_id = game.get('id', 'N/A')
+
+        print(f"\nName: [{game_id}] {name}")
+        print(summary)
+    
+if __name__ == "__main__":
+    main()
