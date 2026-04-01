d419728e5cd6912cce23e21b439beb656bc14b18
AVecesPienso <a.escarcega.r@outlook.com>
2026-04-01 17:39:50 -0600

    refactor: Simplify main to use menu as entry point

--- main.py
+++ main.py
@@ -1,23 +1,9 @@
 import os
 from dotenv import load_dotenv
-from src import igdb_client, news_client, feed
-from datetime import datetime, timedelta
+from src import igdb_client, menu
 
 def main(token, client_id):
-    popular = igdb_client.get_popular_games(token, client_id)
-
-    for game_dict in popular:
-        result = igdb_client.process_feed(game_dict, token, client_id)
-        if result:
-            main_ids = [item["uid"] for item in result["steam_ids"] if item["game"] == game_dict["id"]]
-            franchise_ids = [item["uid"] for item in result["steam_ids"] if item["game"] != game_dict["id"]]
-            steam_ids = (main_ids + franchise_ids)[:10]
-            news = news_client.get_news(steam_ids)
-            years_ago = datetime.now() - timedelta(days=365*2)
-            timestamp = int(years_ago.timestamp())
-            news = [n for n in news if n["date"] > timestamp]
-            if news:
-                feed.print_feed(news, game_dict['name'])
+    menu.show_menu(token, client_id)
     
 if __name__ == "__main__":
     load_dotenv()
