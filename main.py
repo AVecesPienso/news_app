a7680cfd535606691dd1ec1ec8867ceec24f332c
AVecesPienso <a.escarcega.r@outlook.com>
2026-04-03 19:08:41 -0600

    docs: add docstring to main

--- main.py
+++ main.py
@@ -3,6 +3,10 @@ from dotenv import load_dotenv
 from src import igdb_client, menu
 
 def main(token, client_id):
+    """
+    Entry point of the application.
+    Initializes the CLI menu with the provided IGDB credentials.
+    """
     menu.show_menu(token, client_id)
     
 if __name__ == "__main__":
